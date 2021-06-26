7df1c2904401a57ef2deef300b7454a68ad63b3d
Konstantin <krash7087@gmail.com>
2021-06-26 10:27:51 +0300

    Complete the task.

--- 1 Guess the number/ViewController.swift
+++ 1 Guess the number/ViewController.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
     
+    var a = Int.random(in: 0...100)
+    @IBOutlet weak var numberByUser: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBAction func tryButton(_ sender: UIButton) {
+        let b = Int(numberByUser.text!) ?? 0
+        if a == b {
+            print("ugadal")
+            result.text = "Угадал!"
+        } else if a < b {
+            result.text = "Много!"
+        } else if a >  b {
+            result.text = "Мало!"
+        }
+    }
     
-    
-    
-
+    @IBAction func resetButton(_ sender: UIButton) {
+        a = Int.random(in: 0...100)
+    }
 }
 
